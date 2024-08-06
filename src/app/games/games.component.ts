c34afab3da36dfc90b68b872d5de0bfc22343bf2
Matthew Nock <thematt.nock@gmail.com>
2024-08-07 00:21:52 +0100

    starting with three js

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  OnInit,
+  AfterViewInit,
+  Input,
+} from '@angular/core';
+import * as three from 'three';
 
 @Component({
   selector: 'app-games',
   standalone: true,
   imports: [],
   templateUrl: './games.component.html',
-  styleUrl: './games.component.css'
+  styleUrl: './games.component.css',
 })
-export class GamesComponent {
-
-}
+export class GamesComponent {}
